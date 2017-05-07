2efd85c685006fb05400f92ef69db20f42171a77
Krosantos <tymko.tecca@gmail.com>
2017-05-06 23:09:46 -0400

    Commit dat init bby

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public List<Tile> Neighbours;
+    public Tile UU, UL, DL, DD, DR, UR;
+
+    public Sprite Sprite
+    {
+        get
+        {
+            return gameObject.GetComponent<SpriteRenderer>().sprite;
+        }
+        set { gameObject.GetComponent<SpriteRenderer>().sprite = value; }
+    }
+
+    public Unit Unit;
+    public int Height;
+    public bool Passable, Blocking;
+    public Terrain Terrain;
+    public delegate void OnTurn(Tile self);
+}
+
+public enum Terrain
+{
+    Grass,
+    Dirt,
+    Rock,
+    Water,
+    Snow,
+    Ice,
+    Sand,
+    Lava
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    public int MaxHealth, BaseStrength, BaseSpeed, Health, Strength, Speed;
+    public Tile Tile;
+
+    public Sprite Sprite
+    {
+        get
+        {
+            return gameObject.GetComponent<SpriteRenderer>().sprite;
+        }
+        set { gameObject.GetComponent<SpriteRenderer>().sprite = value; }
+    }
+    // public Anisomething something
+
+    public int Team;
+    //False for left, true for right.
+    public bool Facing;
+    public delegate void Move(Unit self);
+    public delegate void Attack(Unit self, Unit victim);
+    public delegate void OnTurn(Unit self);
+    public delegate void OnDeath(Unit self, Unit killer);
+    public delegate void OnAttacked(Unit self, Unit attacker, int damage);
+    public delegate void OnAttack(Unit self, Unit victim);
+    public delegate void AbilityOne(Unit self, Unit target = null);
+    public delegate void AbilityTwo(Unit self, Unit target = null);
+
+    public List<string> Tags;
+}
