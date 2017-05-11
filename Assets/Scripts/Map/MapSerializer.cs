e167f4eb619981ef1b2f57f7cd8f98efac8f7a1c
Krosantos <tymko.tecca@gmail.com>
2017-05-10 22:14:37 -0400

    Map loading scene, fixed serializer bug.

--- Assets/Scripts/Map/MapSerializer.cs
+++ Assets/Scripts/Map/MapSerializer.cs
@@ -1,31 +1,40 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class MapSerializer : MonoBehaviour {
 
-    public GameObject TilePrefab;
+    public GameObject TilePrefab, FillerPrefab;
+    public SpriteDict[] TileDict, FillerDict; 
     public TextAsset Input;
+
+    public void Awake()
+    {
+        DeserializeMap(Input.text);
+    }
 
     public static string SerializeMap(List<Tile> tileList)
     {
         var result = "";
         foreach(var tile in tileList)
         {
-            result += JsonUtility.ToJson(tile)+",";
+            result += JsonUtility.ToJson(tile)+";";
         }
         result = result.Substring(0, result.Length - 1);
         return result;
     }
 
-    public static void DeserializeMap(string serializedMap)
+    public void DeserializeMap(string serializedMap)
     {
-        var split = serializedMap.Split(',');
+        var split = serializedMap.Split(';');
         Tile.AllTiles = new List<Tile>();
         foreach(var raw in split)
         {
-            var tile = JsonUtility.FromJson<Tile>(raw);
+            var prefab = Instantiate(TilePrefab, new Vector3(), Quaternion.identity);
+            var tile = prefab.GetComponent<Tile>();
+            Debug.Log(raw);
+            JsonUtility.FromJsonOverwrite(raw, tile);
             Tile.AllTiles.Add(tile);
+            tile.UpdateEditorSprite(FillerPrefab, TileDict, FillerDict);
         }
     }
 }
