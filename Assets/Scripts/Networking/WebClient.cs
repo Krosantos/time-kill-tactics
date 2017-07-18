1c73178e535863f4ecd468f0c7dce3a78032392c
Tymko Tecca <tymko.tecca@gmail.com>
2017-07-17 22:36:40 -0400

    Stubbly server.

--- Assets/Scripts/Networking/WebClient.cs
+++ Assets/Scripts/Networking/WebClient.cs
@@ -3,7 +3,6 @@ using System.Net;
 using UnityEngine;
 using System.Text;
 using System.Net.Sockets;
-using System.Net;
 using System.Collections;
 using System.Collections.Generic;
 
--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,20 @@
 const net = require('net')
+
+const server = net.createServer(conn => {
+  conn.setEncoding('utf8')
+  console.log('client connected')
+  conn.on('end', () => console.log('client disconnected'))
+  conn.on('data', data => {
+    console.log(data)
+    conn.write('Wut u doin?')
+  })
+  conn.on('error', err => console.log(err))
+})
+
+server.on('error', (err) => {
+  console.log(err)
+  throw err
+})
+server.listen(3000, () => {
+  console.log('Server listening!')
+})
