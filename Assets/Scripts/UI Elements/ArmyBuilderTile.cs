d80d25b390c10f480ea255e0f87aaea9c44fed80
Tymko Tecca <tymko.tecca@gmail.com>
2017-08-29 23:10:16 -0400

    Gently re-grooving. Editor-only tile class is gonna make things less hairy.

---
+++ Assets/Scripts/UI Elements/ArmyBuilderTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class ArmyBuilderTile : MonoBehaviour, IPointerClickHandler
+{
+
+    public int X, Y;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicky Sticky");
+    }
+}
